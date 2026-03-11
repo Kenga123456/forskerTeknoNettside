b5dec4629787320cd9c8539f08197a1d6fbf5591
Kenga123456 <Kenga123456@outlook.com>
2026-03-11 21:28:37 +0100

    slettet rester fra det gamle måten å hente bilder på + endret fullstendig på scrape.js for å hente bildene fra den nye siden og forhåpentligvis legge bildene i bildemappen for prosjektet

--- 2025-2026/scrape.js
+++ 2025-2026/scrape.js
@@ -1,47 +1,41 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const fs = require("fs");
-const path = require("path");
+import fetch from "node-fetch";
+import fs from "fs";
 
-async function fetchHTML(url) {
-  try {
-    const { data } = await axios.get(url);
-    console.log(`Fetched from network: ${url}`);
-    return data;
-  } catch (error) {
-    console.error("Error fetching the HTML:", error);
-    throw error;
+async function login() {
+  const res = await fetch(
+    "https://api.tilsig.com/v1/authentication/authenticate",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: process.env.TILSIG_USER,
+        password: process.env.TILSIG_PASS,
+      }),
+    },
+  );
+
+  const data = await res.json();
+  return data.token;
+}
+
+async function main() {
+  const token = await login();
+  const res = await fetch(
+    "https://api.tilsig.com/api/v1/media/image/by-sensor-id/11813?sensorId=11813",
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    },
+  );
+
+  const data = await res.json();
+
+  for (const img of data) {
+    const imageRes = await fetch(img.url);
+    const buffer = await imageRes.arrayBuffer();
+    const filename = img.timestamp.replace(/[:]/g, "-") + ".jpg";
+    fs.writeFileSync("telenor/images/" + filename, Buffer.from(buffer));
+    console.log("Saved", filename);
   }
 }
 
-function extractImages(html) {
-  const $ = cheerio.load(html);
-  const images = [];
-
-  $("img").each((index, element) => {
-    const src = $(element).attr("src");
-    if (src) images.push(src);
-  });
-
-  return images;
-}
-
-const jsonFilePath = path.join(__dirname, "bilder.json");
-
-function saveToJSON(data) {
-  fs.writeFileSync(jsonFilePath, JSON.stringify(data, null, 2)); //
-  console.log(`Data saved to products.json`);
-}
-
-(async () => {
-  const baseURL =
-    "http://sandkasse-bucket.s3-website-eu-west-1.amazonaws.com/SFHS/";
-
-  try {
-    const html = await fetchHTML(baseURL);
-    const images = extractImages(html);
-    saveToJSON(images);
-  } catch (error) {
-    console.error("Failed to scrape images:", error);
-  }
-})();
+main();
