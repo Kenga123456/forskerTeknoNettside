644fc799f6c6a4d9c7a37789b6af36a473a72362
Kenga123456 <Kenga123456@outlook.com>
2026-03-12 10:45:07 +0100

    prøver å fetche for 7 dager istedenfor 5

--- 2025-2026/scrape.js
+++ 2025-2026/scrape.js
@@ -52,7 +52,7 @@ async function main() {
   const SENSOR_ID = 11810;
 
   const now = new Date();
-  const sevenDaysAgo = new Date(now.getTime() - 5 * 24 * 60 * 60 * 1000);
+  const sevenDaysAgo = new Date(now.getTime() - 7 * 24 * 60 * 60 * 1000);
 
   const from = sevenDaysAgo.toISOString();
   const to = now.toISOString();
